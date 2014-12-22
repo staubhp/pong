16b1341008d0c2d2d75d47d0add11197018fd292
Payton Staub <staubhpa@gmail.com>
2014-12-22 10:49:54 -0500

    collision detection

--- pong.js
+++ pong.js
@@ -19,7 +19,7 @@ var step = function(){
 };
 
 var update = function(){
-	ball.update();
+	ball.update(player.paddle, computer.paddle);
 };
 
 var render = function(){
@@ -70,9 +70,46 @@ function Ball(x, y){
 	this.radius = 5;
 }
 
-Ball.prototype.update = function(){
+Ball.prototype.update = function(paddle1, paddle2){	
 	this.x += this.x_speed;
 	this.y += this.y_speed;
+	var top_x = this.x - 5;
+	var top_y = this.y - 5;
+	var bottom_x = this.x + 5;
+	var bottom_y = this.y + 5;
+
+	if (this.x -5 < 0){
+		this.x = 5;
+		this.x_speed = -this.x_speed;		
+	} else if (this.x +5 > 400) {
+		this. x = 395;
+		this.x_speed = -this.x_speed;
+	}
+
+	if (this.y < 0 || this.y > 600){ //point scored
+		this.x_speed = 0;
+		this.y_speed = 3;
+		this.x = 200;
+		this.y = 300;		
+	}
+
+	//paddle collision
+	if (top_y > 300){
+		if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y &&
+				top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x){
+					this.y_speed = -3;
+					this.x_speed += (paddle1.x_speed / 2);
+					this.y += this.y_speed;
+				}				
+	} else {
+		if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y &&
+				top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x){
+					this.y_speed = 3;
+					this.x_speed += (paddle2.x_speed /2);
+					this.y += this.y_speed;
+				}
+	}
+
 }
 				
 Ball.prototype.render = function(){
